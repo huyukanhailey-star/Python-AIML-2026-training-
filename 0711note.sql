882777dc19d24774eb41b91a6d88470e57da4d4b
Yukan Hu <yukanhuyuku@gmail.com>
2026-07-13 09:48:20 -0400

    0711 note

---
+++ 0711note.sql
@@ -0,0 +1,16 @@
+-- 07-10 note FRIDAY
+-- String
+# RPAD LPAD white padding
+# concat
+# reverse
+# substring with locate / substring_index
+# UPPER LOWER
+# Left Right 
+# CASE WHEN 
+# REGEXP 
+-- Math
+# ^2 is not power
+# SUM AVG RAND CEIL FLOOR ROUND 
+-- Date
+# DATEDIFF DAYOFYEAR month year interval
+# curdate current time date_format
